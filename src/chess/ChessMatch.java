e40f55481ec1959a1f4dfa5657b99de1e1377178
Pedro Paulo <nasseraguiarfilho@gmail.com>
2019-04-20 20:01:06 -0600

    Chess fully working, and showing at the console all pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,7 +3,11 @@ package chess;
 import boardgame.Board;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
+import chess.pieces.Bishop;
+import chess.pieces.Horse;
 
 public class ChessMatch {
 
@@ -19,31 +23,50 @@ public class ChessMatch {
 	public ChessMatch() {
 		turn = 0;
 		currentPlayer = Color.WHITE;
-		board = new Board(8,8);
+		board = new Board(8, 8);
 		initialSetup();
 	}
-	
-	
-	
-	
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
-			
+
 		}
 		return mat;
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('c', 3, new King(board, Color.WHITE));
+		placeNewSetOfPieces(Color.WHITE);
+		placeNewSetOfPieces(Color.BLACK);
 	}
 
+	private void placeNewSetOfPieces(Color color) {
+		int row = 1;
+		if (color == Color.BLACK)
+			row = 8;
+		placeNewPiece('a', row, new Rook(board, color));
+		placeNewPiece('b', row, new Horse(board, color));
+		placeNewPiece('c', row, new Bishop(board, color));
+		placeNewPiece('d', row, new King(board, color));
+		placeNewPiece('e', row, new Queen(board, color));
+		placeNewPiece('f', row, new Bishop(board, color));
+		placeNewPiece('g', row, new Horse(board, color));
+		placeNewPiece('h', row, new Rook(board, color));
+		if (color == Color.BLACK)
+			row--;
+		else
+			row++;
+		placePawns(color, row);
+	}
 
-
+	private void placePawns(Color color, int row) {
+		for (int i = 0; i < 8; i++) {
+			placeNewPiece(((char) ('a' + (char) i)), row, new Pawn(board, color));
+		}
+	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
